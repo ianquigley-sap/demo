3766b211e5e3816081f764f3b404745891c5096f
Ian Quigley <ian.quigley@sap.com>
2020-07-09 06:57:19 +0100

    fix(app-gen): to be edited

--- catalogapp/dist/Component-dbg.js
+++
@@ -1,7 +0,0 @@
-sap.ui.define(['sap/suite/ui/generic/template/lib/AppComponent'], function(AppComponent) {
-    return AppComponent.extend('com.catafdd.com.catalogapp.Component', {
-        metadata: {
-            manifest: 'json'
-        }
-    });
-});
--- catalogapp/dist/Component.js
+++
@@ -1,1 +0,0 @@
-sap.ui.define(["sap/suite/ui/generic/template/lib/AppComponent"],function(e){return e.extend("com.catafdd.com.catalogapp.Component",{metadata:{manifest:"json"}})});
