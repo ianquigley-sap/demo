49f9d462a81c8d171a0e5c63dd6703341c4c917b
Ian Quigley <ian.quigley@sap.com>
2020-07-09 05:53:42 +0000

    init commit

---
+++ catalogapp/webapp/Component.js
@@ -0,0 +1,7 @@
+sap.ui.define(['sap/suite/ui/generic/template/lib/AppComponent'], function(AppComponent) {
+    return AppComponent.extend('com.catafdd.com.catalogapp.Component', {
+        metadata: {
+            manifest: 'json'
+        }
+    });
+});
