a012293f9e069da421c306e30160a10e9a52f08e
Ian Quigley <ian.quigley@sap.com>
2020-07-09 06:03:26 +0000

    initial build

---
+++ catalog/dist/Component-dbg.js
@@ -0,0 +1,7 @@
+sap.ui.define(['sap/suite/ui/generic/template/lib/AppComponent'], function(AppComponent) {
+    return AppComponent.extend('fdsafd.fdasfsd.com.catalog.Component', {
+        metadata: {
+            manifest: 'json'
+        }
+    });
+});
