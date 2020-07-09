f03102801c329038e4346886cde9af02a7e5e733
Ian Quigley <ian.quigley@sap.com>
2020-07-09 07:13:12 +0100

    fix(app-gen): to be edited

--- catalog/webapp/Component.js
+++
@@ -1,7 +0,0 @@
-sap.ui.define(['sap/suite/ui/generic/template/lib/AppComponent'], function(AppComponent) {
-    return AppComponent.extend('fdsafd.fdasfsd.com.catalog.Component', {
-        metadata: {
-            manifest: 'json'
-        }
-    });
-});
